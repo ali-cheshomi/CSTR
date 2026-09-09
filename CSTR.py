c628f113da3a0b0452fcb8e3c22c8588eb6735d7
Ali Cheshomi <88206576+ali-cheshomi@users.noreply.github.com>
2026-09-09 10:04:50 -0700

    Update CSTR.py

--- CSTR.py
+++ CSTR.py
@@ -1,4 +1,4 @@
-# Copyright 2021 MR_AC. All rights reserved.
+# Copyright 2026 MR_AC. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
